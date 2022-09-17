36d90eec3e80806a591ab9f132175f29fc199a14
julianchoi007 <choijulian007@gmail.com>
2022-09-17 15:14:42 -0400

    added the api for random

--- backend/app.js
+++ backend/app.js
@@ -65,6 +65,18 @@ app.get("/list", (req, res) => {
     });
 });
 
+app.get("/random", (req, res) => {
+  Recipe.sync({
+    force: false,
+  })
+    .then(() => {
+      return Recipe.findAll({ order: Sequelize.literal('random()'), limit: 1 });
+    })
+    .then((recipe) => {
+      res.send(recipe);
+    });
+});
+
 app.post("/add", (req, res) => {
   Recipe.sync({
     force: false,
