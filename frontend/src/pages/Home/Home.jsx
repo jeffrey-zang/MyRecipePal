bc99734dcd98ba305161e11e8412822a9e16fc03
Jeffrey Zang <zangj4548@wrdsb.ca>
2022-09-17 03:27:10 -0400

    homepage

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import {
-    Box,
+    Button,
+    Center
 } from '@chakra-ui/react'
 import './home.css'
+import diet from '../../assets/diet.png'
 
 const Home = () => {
   return (
+    <>
     <div className = 'container'>
-        This is the Box
+        Title
+        <Center><img src = {diet} alt = 'dietimg'></img></Center>
+        <h1 style = {{fontSize: 'max(3vw, 30px)'}}>Change your life through diet</h1>
+        <Center>
+            <Button className = 'btn' colorScheme='blue' marginTop = '25px' marginBottom = '-20px'>Get Started</Button>
+        </Center>
+        <Button className = 'btn' colorScheme='blue'>Post a Recipe</Button>
     </div>
+    </>
     )
 }
 
