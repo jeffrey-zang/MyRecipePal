dc82c2cf5ad13c1065801cdfb5682440c7933b39
Jeffrey Zang <zangj4548@wrdsb.ca>
2022-09-17 09:06:15 -0400

    form for name

--- frontend/src/pages/AddRecipe/AddRecipe.jsx
+++ frontend/src/pages/AddRecipe/AddRecipe.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import {React, useState} from 'react'
+
+import {
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    Input,
+    Center,
+    Button
+} from '@chakra-ui/react'  
 
 const AddRecipe = () => {
-  return (
-    <div>add some recipes</div>
-  )
+    const [input, setInput] = useState('')
+
+    const handleInputChange = (e) => setInput(e.target.value)
+
+    const isError = input === ''
+  
+    return (
+        <>
+            <FormControl isInvalid={isError}>
+                <FormLabel style = {{textAlign: 'center', fontSize: 'max(1.5vw, 15px)', marginTop:'25px'}}>Name</FormLabel>
+                <Center>
+                    <Input     
+                        errorBorderColor='red.300'
+                        placeholder='Here is a sample placeholder'
+                        width = '40%' 
+                        borderColor='green.400'
+                        onChange={handleInputChange}
+                    />
+                </Center>
+                <Center>
+                    <FormErrorMessage>Name is required.</FormErrorMessage>
+                </Center>
+                <Center>
+                    <Button
+                        mt={4}
+                        colorScheme='teal'
+                        type='submit'
+                        >
+                        Submit
+                    </Button>
+                </Center>
+            </FormControl>    
+        </>
+    )
 }
 
 export default AddRecipe
