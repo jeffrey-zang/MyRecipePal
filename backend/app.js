213be241f31ca61bbb36d5ad57dcc65528cd3dc9
Jeffrey Zang <zangj4548@wrdsb.ca>
2022-09-17 00:40:29 -0400

    inital commit of stuff
    Co-authored-by: julianchoi007 <julianchoi007@users.noreply.github.com>
    Co-authored-by: Danny Lu <danny.lu075@gmail.com>

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+const { Client } = require("pg");
+
+(async () => {
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    application_name: "$ docs_quickstart_node"
+  });
+
+  const statements = [
+    // CREATE the messages table
+    "CREATE TABLE IF NOT EXISTS messages (id UUID PRIMARY KEY DEFAULT gen_random_uuid(), message STRING)",
+    // INSERT a row into the messages table
+    "INSERT INTO messages (message) VALUES ('Hello world!')",
+    // SELECT a row from the messages table
+    "SELECT message FROM messages",
+  ];
+
+  try {
+    // Connect to CockroachDB
+    await client.connect();
+    for (let n = 0; n < statements.length; n++) {
+      let result = await client.query(statements[n]);
+      if (result.rows[0]) { console.log(result.rows[0].message); }
+    }
+    await client.end();
+  } catch (err) {
+    console.log(`error connecting: ${err}`);
+  }
+
+  // Exit program
+  process.exit();
+})().catch((err) => console.log(err.stack));
