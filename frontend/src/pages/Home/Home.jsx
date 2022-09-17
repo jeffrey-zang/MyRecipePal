ab5827fbc36ffc877b62a5e846646b38e33afc07
Jeffrey Zang <zangj4548@wrdsb.ca>
2022-09-17 04:18:28 -0400

    our inspiration

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -5,18 +5,26 @@ import {
 } from '@chakra-ui/react'
 import './home.css'
 import logo from '../../assets/logo.png'
+import diet from '../../assets/diet.png'
 
 const Home = () => {
   return (
     <>
     <div className = 'container'>
         Title
-        <Center><img src = {logo} alt = 'dietimg'></img></Center>
+        <Center><img className = 'logo' src = {logo} alt = 'logo'></img></Center>
         <h1 style = {{fontSize: 'max(3vw, 30px)'}}>Change your life through diet</h1>
         <Center>
             <Button className = 'btn' colorScheme='green' marginTop = '25px' marginBottom = '-20px'>Get Started</Button>
         </Center>
         <Button className = 'btn' colorScheme='green'>Post a Recipe</Button>
+    </div>
+    <div className = 'container' style = {{padding: '0px 80px'}}>
+        Our inspiration
+        <p className = 'text'>
+            As university students, we all understand that one of the biggest struggles of living by yourself is cooking food. We wanted to make it easy to find and craft new recipes, while still eating healthy. 
+        </p>
+        <Center><img src = {diet} alt = 'dietimg' className = 'img'></img></Center>
     </div>
     </>
     )
