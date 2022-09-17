f3714d4392d14d5c589c42cdfc917374f767929a
Jeffrey Zang <zangj4548@wrdsb.ca>
2022-09-17 03:49:00 -0400

    add logo

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -4,19 +4,19 @@ import {
     Center
 } from '@chakra-ui/react'
 import './home.css'
-import diet from '../../assets/diet.png'
+import logo from '../../assets/logo.png'
 
 const Home = () => {
   return (
     <>
     <div className = 'container'>
         Title
-        <Center><img src = {diet} alt = 'dietimg'></img></Center>
+        <Center><img src = {logo} alt = 'dietimg'></img></Center>
         <h1 style = {{fontSize: 'max(3vw, 30px)'}}>Change your life through diet</h1>
         <Center>
-            <Button className = 'btn' colorScheme='blue' marginTop = '25px' marginBottom = '-20px'>Get Started</Button>
+            <Button className = 'btn' colorScheme='green' marginTop = '25px' marginBottom = '-20px'>Get Started</Button>
         </Center>
-        <Button className = 'btn' colorScheme='blue'>Post a Recipe</Button>
+        <Button className = 'btn' colorScheme='green'>Post a Recipe</Button>
     </div>
     </>
     )
